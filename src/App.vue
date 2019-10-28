e5e531c2b9d24486409694776232d02df16bb63d
jgierlach <jegierlach@gmail.com>
2019-10-27 22:50:40 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <Navbar />
+    <v-content>
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <Footer />
+  </v-app>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+import Footer from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+    Footer
+  },
+  data: () => ({
+    //
+  })
+}
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+  <v-footer color="primary" app>
+    <span class="white--text">&copy; 2019</span>
+  </v-footer>
+</template>
