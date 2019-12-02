df227a3022af1ab563c89b92475cd9d980774112
jgierlach <jegierlach@gmail.com>
2019-12-02 16:08:10 -0500

    Added transition

--- src/views/AnalysisOfProblem.vue
+++ src/views/AnalysisOfProblem.vue
@@ -1,4 +1,5 @@
 <template>
+<transition name="fade" appear>
   <div class="container">
     <p><strong>1.)</strong> The prolific volume of high quality content that Campus Ministries produces is truly impressive. CM produces places for people to gather,  resources to help nurture, and the tools to send each individual off into the world as a global citizen. It then disseminates this content across 11 different outlets and 6 different platforms that Campus Ministries currently uses to distribute the media it creates. However, the uncentralized publication of content presents a challenge of accessibility when it comes to the various publics that Campus Ministries is hoping to reach. 62% of our survey respondents do not follow any of the Campus Ministries Social Media (i.e. Campus Ministries on Twitter, Instagram, Campus Ministries Athletics and Team X, Y, or Z on Instagram). The distribution of Campus Ministries message and mission would be more effective if the channels were consolidated and branded in such a way that all their media was accessible in one place.</p>
 
@@ -11,6 +12,7 @@
 	  <p><strong>5.)</strong> Though Campus Ministries is our main focus, the department wouldn't be what it is or what it will be without the leadership from Trgve Johnson. To improve the perception of Hope College Campus Ministries it will need to be an internal change but also an external one. Trgve Johnson represents himself and Hope College. However Trgve does not have a brand that represents him or Hope College. In regards to all of Trygve Johnson’s speaking engagements (on and off campus), not having a brand established is missed opportunity for further engagements. It is setting back the possibility of the establishment of Campus Ministries becoming a five star program.</p>
 
   </div>
+  </transition>
 </template>
 
 <script>
