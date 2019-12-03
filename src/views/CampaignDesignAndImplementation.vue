958a3d81aa28060d4eca9a240c22c7b31f78001b
jgierlach <jegierlach@gmail.com>
2019-12-03 16:51:06 -0500

    Fixed scroll problem

--- src/views/CampaignDesignAndImplementation.vue
+++ src/views/CampaignDesignAndImplementation.vue
@@ -2,9 +2,9 @@
   <transition name="fade" appear>
     <div class="container">
       <p>
-        <strong>Program Goal:</strong> Communicate Hope College Campus Ministries
-        is a top-tier Christian community and influences admissions and public
-        perceptions of Hope College in a positive way.
+        <strong>Program Goal:</strong> Communicate Hope College Campus
+        Ministries is a top-tier Christian community and influences admissions
+        and public perceptions of Hope College in a positive way.
       </p>
 
       <p>
@@ -33,16 +33,16 @@
       <br />
 
       <p>
-        <strong>Objective 2:</strong> To increase by 5% admissions numbers by May
-        1, 2021
+        <strong>Objective 2:</strong> To increase by 5% admissions numbers by
+        May 1, 2021
       </p>
 
       <p>
         The second objective will seek to increase Hope College enrollment. Not
         Just a Church will implement a new portion of the tour guide training
-        bi-annually at the Admissions Office. Campus Ministries social media will
-        be presented to prospective and incoming freshman to increase engagement
-        on social media.
+        bi-annually at the Admissions Office. Campus Ministries social media
+        will be presented to prospective and incoming freshman to increase
+        engagement on social media.
       </p>
 
       <p>
@@ -50,24 +50,24 @@
       </p>
 
       <p>
-        <strong>#1:</strong> Campus Ministries staff attend the annual tour guide
-        training
+        <strong>#1:</strong> Campus Ministries staff attend the annual tour
+        guide training
       </p>
 
       <p>
         A recent drop in enrollment numbers doesn’t seem that it would influence
         Campus Ministries, but as a department of the college, if the College
-        hurts each department feels a small piece of that deficit. What if Campus
-        Ministries could be the key factor in helping increase enrollment numbers
-        again? Tour guides for colleges can make or break the decision of a
-        prospective student to accept or deny attendance. We suggest that Campus
-        Ministries attend the annual tour guide training each semester to make
-        sure that Campus Ministries is being pitched properly. We want to make
-        sure that prospective students are being told the best things about Hope
-        College I.e. Campus Ministries and can make a fully educated decision.
-        Campus Ministries would share with the tour guides at the training what
-        they want to be told to prospective students to hopefully sell them on
-        Hope.
+        hurts each department feels a small piece of that deficit. What if
+        Campus Ministries could be the key factor in helping increase enrollment
+        numbers again? Tour guides for colleges can make or break the decision
+        of a prospective student to accept or deny attendance. We suggest that
+        Campus Ministries attend the annual tour guide training each semester to
+        make sure that Campus Ministries is being pitched properly. We want to
+        make sure that prospective students are being told the best things about
+        Hope College I.e. Campus Ministries and can make a fully educated
+        decision. Campus Ministries would share with the tour guides at the
+        training what they want to be told to prospective students to hopefully
+        sell them on Hope.
       </p>
 
       <p>
@@ -108,13 +108,13 @@
         Hope College Public Affairs and Marketing is a key factor to success of
         departments on campus. Modeled after the position within the Development
         Office and Family and Alumni Engagement Office we as a team want Campus
-        Ministries to create a communications specialist to be the liaison between
-        the vision of Campus Ministries staff and Public Affairs and Marketing.
-        Through research done on Hope’s Campus and surrounding colleges
-        organization’s having a Communication Specialist is critical to the
-        success of the department. Creating new channels to get across the message
-        you as a program are wanting to convey is the winning element to a
-        successful program.
+        Ministries to create a communications specialist to be the liaison
+        between the vision of Campus Ministries staff and Public Affairs and
+        Marketing. Through research done on Hope’s Campus and surrounding
+        colleges organization’s having a Communication Specialist is critical to
+        the success of the department. Creating new channels to get across the
+        message you as a program are wanting to convey is the winning element to
+        a successful program.
       </p>
 
       <p>
@@ -124,14 +124,14 @@
 
       <p>
         Over the next ten years Campus Ministries will no longer be developing
-        into a gold star program but growing its roots into the department it has
-        worked hard at to become. To put the star into the gold star program,
-        Campus Ministries should host a leadership conference to help other
-        programs make the same growth Hope College Campus Ministries has in the
-        past ten years. Though many programs hope to replicate Campus Ministries
-        by what they see as an example, teaching the new generation of educators
-        how to accomplish this ambitious goal will be true success for the
-        program.
+        into a gold star program but growing its roots into the department it
+        has worked hard at to become. To put the star into the gold star
+        program, Campus Ministries should host a leadership conference to help
+        other programs make the same growth Hope College Campus Ministries has
+        in the past ten years. Though many programs hope to replicate Campus
+        Ministries by what they see as an example, teaching the new generation
+        of educators how to accomplish this ambitious goal will be true success
+        for the program.
       </p>
 
       <p>
@@ -140,20 +140,20 @@
       </p>
 
       <p>
-        In the end the ultimate goal of Campus Ministries is to merge with Western
-        Seminary and create Hope College Theological Center. Being able further
-        educate the undergraduate minds of Hope College as well as minds from
-        other colleges would be the ultimate fulfillment as a department. This
-        would bring both national recognition for Campus Ministries but also Hope
-        College. This would be the ultimate goal of the entire program and
-        campaign we have created.
+        In the end the ultimate goal of Campus Ministries is to merge with
+        Western Seminary and create Hope College Theological Center. Being able
+        further educate the undergraduate minds of Hope College as well as minds
+        from other colleges would be the ultimate fulfillment as a department.
+        This would bring both national recognition for Campus Ministries but
+        also Hope College. This would be the ultimate goal of the entire program
+        and campaign we have created.
       </p>
 
       <br />
 
       <p>
-        <strong>Objective 4:</strong> To increase by 5% Trygve’s brand recognition
-        by August 31, 2020
+        <strong>Objective 4:</strong> To increase by 5% Trygve’s brand
+        recognition by August 31, 2020
       </p>
 
       <p>
